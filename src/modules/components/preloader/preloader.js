3eaa39f89f30e74fd8f8a4f882535d29d903b921
unknown <375445433672@yandex.by>
2022-10-29 00:39:18 +0300

    fix: preloader

--- src/modules/components/preloader/preloader.js
+++ src/modules/components/preloader/preloader.js
@@ -8,18 +8,35 @@ class Preloader {
   }
   addContent() {
     this.element.innerHTML = `
-      <div class="load-wrapper">
-        <h1 class="preloader-title">ITBOAT</h1>
-        <div class="preloader"></div>
+     <div class="center">
+      <div class="sk-cube-grid">
+        <div class="sk-cube sk-cube-1"></div>
+        <div class="sk-cube sk-cube-2"></div>
+        <div class="sk-cube sk-cube-3"></div>
+        <div class="sk-cube sk-cube-4"></div>
+        <div class="sk-cube sk-cube-5"></div>
+        <div class="sk-cube sk-cube-6"></div>
+        <div class="sk-cube sk-cube-7"></div>
+        <div class="sk-cube sk-cube-8"></div>
+        <div class="sk-cube sk-cube-9"></div>
       </div>
     `;
   }
+  //  <h1 class="preloader-title">ITBOAT</h1>
+  // <div class="load-wrapper">
+
+  //       <div class="preloader"></div>
+  //     </div>
   stopPreloader() {
+    const center = document.querySelector('.center');
     const loader = document.querySelector('.load');
+    center.classList.add('load-hidden');
     loader.classList.add('load-hidden');
   }
   removePreloader() {
+    const center = document.querySelector('.center');
     const loader = document.querySelector('.load');
+    center.classList.add('load-none');
     loader.classList.add('load-none');
   }
 }
